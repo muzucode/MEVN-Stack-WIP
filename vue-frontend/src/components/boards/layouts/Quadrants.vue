3c903be14bdfcb0163dbae187b428157d8d01b4d
muzucode <79173451+muzucode@users.noreply.github.com>
2021-03-05 00:06:19 -0500

    Finishing touches on baseball frame

--- vue-frontend/src/components/boards/layouts/Quadrants.vue
+++ vue-frontend/src/components/boards/layouts/Quadrants.vue
@@ -19,37 +19,68 @@
             </div>
             <div class="fcontent-container">
               <div class="fcontent-1">
-              <div class="current-date">
-                March 4, 2021
-              </div>
-              <div class="current-game">
-                <img class="team-img" src="../../../assets/baseball-logos/phillies.png"/>
-                <div>vs</div>
-                <img class="team-img" src="../../../assets/baseball-logos//dodgers.png"/>
-              </div>
-              <div class="boxscore">
+                <div class="content-header">
+                  March 4, 2021
+                </div>
+                <div class="current-game">
+                  <img class="team-img" src="../../../assets/baseball-logos/phillies.png"/>
+                  <div>vs</div>
+                  <img class="team-img" src="../../../assets/baseball-logos//dodgers.png"/>
+                </div>
+                <div class="boxscore">
                 <table>
                   <tr id="inning">
                     <td class="row-label">Inning</td>
                     <td v-for="index in 9" :key="index">{{index}}</td>
+                    <td>Totals</td>
                   </tr>
                   <tr id="away">
                     <td class="row-label">Away</td>
-                    <td v-for="index in 9" :key="index"></td>
+                    <td v-for="index in 10" :key="index"></td>
                   </tr>
                   <tr id="home">
                     <td class="row-label">Home</td>
-                    <td v-for="index in 9" :key="index"></td>
+                    <td v-for="index in 10" :key="index"></td>
                   </tr>
                   <tr id="hits">
                     <td class="row-label">Hits</td>
-                    <td v-for="index in 9" :key="index"></td>
+                    <td v-for="index in 10" :key="index"></td>
                   </tr>
                 </table>
+                </div>
               </div>
+
+            <div class="fcontent-divider">
+
             </div>
+
+
               <div class="fcontent-2">
-              
+                <div class="content-header">
+                  League Scores
+                </div>
+                <div class="league-scores">
+                  <table>
+                    <tr v-for="i in 7" :key="i">
+                      <td class="league-scores-outer-td">
+                        <tr>
+                          <td>PHI</td>
+                        </tr>
+                        <tr>
+                          <td>PIT</td>
+                        </tr>
+                      </td>
+                      <td class="league-scores-outer-td">
+                        <tr>
+                          <td>5</td>
+                        </tr>
+                        <tr>
+                          <td>6</td>
+                        </tr>
+                      </td>
+                    </tr>
+                  </table>
+                </div>
               </div>
             </div>
             
@@ -78,26 +109,47 @@
 </template>
 
 <style>
+.fcontent-divider{
+  width:.3vw;
+  background-color:rgb(0, 0, 0);
+}
+.league-scores-outer-td > tr > td{
+  padding-left:10px;
+}
+.league-scores{
+  height:100%;
+  overflow:auto;
+  border:solid 0px blue;
+}
+.league-scores > table{
+  width:100%;
+  color:black;
+  height:100%;
+
+}
+.league-scores > table > tr > td{
+  border-bottom: solid 1px black;
+  font-size: .6vw;
+}
 .row-label{
   width:4vw;
 }
-td{
-  font-size:.7vw;
-}
-table{
+.boxscore > table{
   width:100%;
   color:black;
+
 }
-table > tr > td{
-  border: solid 1px black;
+.boxscore > table > tr > td{
+  border: solid 0px black;
+  font-size:.7vw;
 }
 .boxscore{
   display:flex;
   overflow:auto;
-  border:solid 5px blue;
+  border-top:solid 2px rgb(0, 0, 0);
 }
 .team-img{
-  height:10vh;
+  height:11vh;
 }
 .current-game > div {
   font-size: 2.5em;
@@ -106,7 +158,7 @@ table > tr > td{
   font-weight: bold;
 }
 .current-game{
-  border: solid 5px greenyellow;
+  border: solid 0px rgb(0, 0, 0);
   color:white;
   font-size:1vw;
   display:flex;
@@ -116,50 +168,52 @@ table > tr > td{
   align-items: center;
   justify-content: center;
 }
-.current-date{
-  border: solid 5px rgb(255, 184, 104);
+.content-header{
+  border: solid 0px rgb(255, 184, 104);
+  border-bottom:solid .3vw rgb(0, 0, 0);
+  background-color:rgb(255, 148, 148);
+  font-weight:bold;
   color:black;
   font-size:1vw;
-  padding:5px;
+  padding:10px 10px 10px 10px;
 }
 
 .fcontent-1{
-  border:solid 5px red;
+  border: solid 0px rgb(1, 214, 221);
   display:flex;
   flex-flow: column;
   height:100%;
   background-color: white;
+  flex-grow:1;
 }
 .fcontent-2{
-  border:solid 5px rgb(1, 214, 221);
+  border:solid 0px rgb(1, 214, 221);
   display:flex;
   flex-flow: column;
-  width:100%;
   height:100%;
   background-color: white;
+  float:right;
 }
 .fcontent-container{
-  border:solid 5px red;
+  border:solid 0px red;
   display:flex;
   flex-flow: row;
-  height:85%;
+  height:90%;
   background-color: white;
 }
 .frow{
   height:100%;
   display:flex;
   flex-flow: row;
-
 }
 .ftab{
-  border-right: solid 2px #b9c4cf;
+  border-right: solid 2px #000000;
   display:flex;
   flex-flow: row;
   padding:0px 10px 0px 10px;
   background-color: rgb(36, 39, 70);
-  font-weight: bold;
   height:100%;
-  font-size:1vw;
+  font-size:.8vw;
   align-items: center;
 }
 .ftab:hover{
@@ -167,13 +221,13 @@ table > tr > td{
   cursor:pointer;
 }
 .fcontainer{
-  border-bottom:solid 5px #b9c4cf;
-  height:15%;
+  border-bottom:solid 5px #000000;
+  height:10%;
   color:white;
   align-items: left;
 }
 #q1{
-  border:solid 5px #b9c4cf;
+  border:solid 5px #000000;
 }
 #boxy{
   border: 5px solid rgb(199, 96, 96);
